b6b8a136f6f5ba64578e2e29c62b6eab60c574b5
Paul Amazona <whatevergeek@gmail.com>
2022-05-08 07:31:32 +0000

    refactored to use textbox and button

--- py/time_info.py
+++ py/time_info.py
@@ -1,10 +1,16 @@
-from js import document
+import js
+from pyodide import create_proxy
 
 from datetime import datetime
 now = datetime.now()
 dt = now.strftime("%m/%d/%Y, %H:%M:%S")
 
-document.getElementById("lblOut").innerHTML = "<b>Labeled Output</b>"
+def print_result(evt):
+    name = js.txtName.value
+    js.document.getElementById("lblOut").innerHTML = f"Hello <b>{name}</b>!!!"
+
+print_result_proxy = create_proxy(print_result)
+js.document.getElementById("btnClick").addEventListener ('click', print_result_proxy)
 
 print(f"This is the current date and time, as computed by Python: {dt}")
 for i in range(0,5):
